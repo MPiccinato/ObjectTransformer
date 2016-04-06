f333f3bef12ac6d0c6ce1b1219bbd815ede620df
Piccinato <mpiccinato@gmail.com>
2016-04-06 10:31:33 -0400

    Add options for custom date format/formatter on NSDateTransformer

--- Source/Transformers/NSDateTransformer.swift
+++ Source/Transformers/NSDateTransformer.swift
@@ -40,3 +40,67 @@ public class NSDateTransformer: Transformer<NSDate, Any> {
         throw TransformerError.Failed
     }
 }
+
+public class NSDateCustomFormatTransformer: Transformer<NSDate, Any> {
+    
+    public typealias ObjectType = NSDate
+    public typealias JSONType = Any
+    
+    let formatter: NSDateFormatter
+    
+    public init(format: String) {
+        self.formatter = NSDateFormatter()
+        formatter.dateFormat = format
+        formatter.timeZone = NSTimeZone(name: "UTC")
+    }
+    
+    public override func toObject(from: Any) throws -> NSDate {
+        
+        if let value = from as? String {
+            if let date = self.formatter.dateFromString(value) {
+                return date
+            }
+        }
+        
+        throw TransformerError.Failed
+    }
+    
+    public override func toJSON(object: NSDate?) throws -> Any {
+        if let date = object {
+            return self.formatter.stringFromDate(date)
+        }
+        
+        throw TransformerError.Failed
+    }
+}
+
+public class NSDateTransformerCustom: Transformer<NSDate, Any> {
+    
+    public typealias ObjectType = NSDate
+    public typealias JSONType = Any
+    
+    let formatter: NSDateFormatter
+    
+    public init(formatter: NSDateFormatter) {
+        self.formatter = formatter
+    }
+    
+    public override func toObject(from: Any) throws -> NSDate {
+        
+        if let value = from as? String {
+            if let date = self.formatter.dateFromString(value) {
+                return date
+            }
+        }
+        
+        throw TransformerError.Failed
+    }
+    
+    public override func toJSON(object: NSDate?) throws -> Any {
+        if let date = object {
+            return self.formatter.stringFromDate(date)
+        }
+        
+        throw TransformerError.Failed
+    }
+}
